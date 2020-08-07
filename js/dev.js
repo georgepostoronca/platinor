99502c52b4899c0e8fce76ea3e05d33c1e629088
George <george.postoronca@gmail.com>
2020-08-07 18:07:21 +0300

    Asana fix v.11

--- js/dev.js
+++ js/dev.js
@@ -386,7 +386,7 @@ if (document.querySelector(".swiper-container")) {
 
       // breakpoint where swiper will be destroyed
       // and switches to a dual-column layout
-      const breakpoint = window.matchMedia('(min-width:768px)');
+      const breakpoint = window.matchMedia('(min-width:769px)');
 
       // keep track of swiper instances to destroy later
       let mySwiper;
@@ -929,7 +929,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         errorPrefix: 'bouncer-error_', // Prefix used for error message IDs
         patterns: {
           email: /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/,
-          password: /(?=.*\d)(?=.*[a-zа-яё|A-ZА-ЯЁ]).{8,}/,
+          // password: /(?=.*\d)(?=.*[a-zа-яё|A-ZА-ЯЁ]).{8,}/,
+          password: /[\d\w\W\D\d].{7,}/,
           tel: /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/,
           phone: /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/
         },
